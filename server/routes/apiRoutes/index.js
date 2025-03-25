6009255facb2fc49c4e7388b4ab3c335093c5445
paul88vera <paul88vera@gmail.com>
2025-03-24 21:40:38 -0500

    corrected schema for MySQL DB

--- server/routes/apiRoutes/index.js
+++ server/routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Client = require("./clients");
+const Company = require("./company");
 
 // EXAMPLE ->   router.use(require('./route_name', route_file))
 router.use("/clients", Client);
+router.use("/company", Company);
 
 module.exports = router;
